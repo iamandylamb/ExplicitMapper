16bf75ca88ae1c3610203454a70b7bbd8910fde3
iamandylamb <54452911+iamandylamb@users.noreply.github.com>
2020-02-05 12:15:07 +0000

    Create Password.cs

---
+++ example/mapping/models/Password.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Example.ExplicitMapper
+{
+    public class Password
+    {
+        public string HashedPassword { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
